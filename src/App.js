a33b13e036ca5059e98afa900e669ccbc32260db
David <david.campos.solorio@gmail.com>
2021-08-04 16:56:08 -0500

    project start

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/styles/App.scss';
+
+const App = () => {
+    return (
+        <div>
+            Hello world 2
+        </div>
+    )
+}
+
+export default App
